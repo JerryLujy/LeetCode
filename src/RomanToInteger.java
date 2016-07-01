99a719caee69d558dfc53e989014cfd1a56d1099
Jieyu Lu <jerrylujieyu@gmail.com>
2016-06-30 21:41:34 -0400

    Added problem 13.

---
+++ src/RomanToInteger.java
@@ -0,0 +1,31 @@
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * <h1>13. Roman to Integer</h1>
+ * Given a roman numeral, convert it to an integer. Input is guaranteed to be within the range from 1 to 3999.
+ *
+ * @author Jerry
+ * @since 6/30/16
+ */
+public class RomanToInteger {
+    public int romanToInt(String s) {
+        Map<Character, Integer> map = new HashMap<>();
+        map.put('I', 1);
+        map.put('V', 5);
+        map.put('X', 10);
+        map.put('L', 50);
+        map.put('C', 100);
+        map.put('D', 500);
+        map.put('M', 1000);
+        int ans = map.get(s.charAt(s.length() - 1));
+        for (int i = s.length() - 2; i >= 0; i--) {
+            if (map.get(s.charAt(i)) < map.get(s.charAt(i + 1))) {
+                ans -= map.get(s.charAt(i));
+            } else {
+                ans += map.get(s.charAt(i));
+            }
+        }
+        return ans;
+    }
+}
