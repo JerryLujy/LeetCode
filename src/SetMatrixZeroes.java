41dfedacb817ef9eda1761f35131395775c9d08f
Jieyu Lu <jerrylujieyu@gmail.com>
2016-08-31 22:07:38 -0400

    Added problem 73.

---
+++ src/SetMatrixZeroes.java
@@ -0,0 +1,55 @@
+/**
+ * <h1>73. Set Matrix Zeroes</h1>
+ * Given a m x n matrix, if an element is 0, set its entire row and column to 0. Do it in place.
+ *
+ * @author Jerry
+ * @since 8/31/16
+ */
+public class SetMatrixZeroes {
+    public void setZeroes(int[][] matrix) {
+        if (matrix == null || matrix.length == 0) {
+            return;
+        }
+        int m = matrix.length;
+        int n = matrix[0].length;
+        boolean isFirstRowZero = false;
+        boolean isFirstColZero = false;
+        for (int i = 0; i < m; i++) {
+            if (matrix[i][0] == 0) {
+                isFirstColZero = true;
+                break;
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            if (matrix[0][i] == 0) {
+                isFirstRowZero = true;
+                break;
+            }
+        }
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (matrix[i][j] == 0) {
+                    matrix[0][j] = 0;
+                    matrix[i][0] = 0;
+                }
+            }
+        }
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (matrix[0][j] == 0 || matrix[i][0] == 0) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+        if (isFirstRowZero) {
+            for (int i = 0; i < n; i++) {
+                matrix[0][i] = 0;
+            }
+        }
+        if (isFirstColZero) {
+            for (int i = 0; i < m; i++) {
+                matrix[i][0] = 0;
+            }
+        }
+    }
+}
