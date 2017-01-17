6d5afcc87e0dffe7d020c776cb7efd7321bf9013
Jieyu Lu <jerrylujieyu@gmail.com>
2017-01-17 18:51:51 -0500

    Added problem 114.

---
+++ src/FlattenBinaryTreeToLinkedList.java
@@ -0,0 +1,65 @@
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+/**
+ * <h1>114. Flatten Binary Tree to Linked List</h1>
+ * Given a binary tree, flatten it to a linked list in-place.
+ * <p>
+ * For example, Given
+ * <pre>
+ *      1
+ *     / \
+ *    2   5
+ *   / \   \
+ *  3   4   6
+ * </pre>
+ * The flattened tree should look like:
+ * <pre>
+ * 1
+ *  \
+ *   2
+ *    \
+ *     3
+ *      \
+ *       4
+ *        \
+ *         5
+ *          \
+ *           6
+ * </pre>
+ *
+ * @author Jerry
+ * @since 1/17/17
+ */
+public class FlattenBinaryTreeToLinkedList {
+    public void flatten(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        Deque<TreeNode> stack = new ArrayDeque<>();
+        stack.offerLast(root);
+        while (!stack.isEmpty()) {
+            TreeNode curr = stack.pollLast();
+            if (curr.right != null) {
+                stack.offerLast(curr.right);
+            }
+            if (curr.left != null) {
+                stack.offerLast(curr.left);
+                curr.left = null;
+            }
+            if (!stack.isEmpty()) {
+                curr.right = stack.peekLast();
+            }
+        }
+    }
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+}
