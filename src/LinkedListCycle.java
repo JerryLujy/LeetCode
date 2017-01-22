04f17d774a82b8cc004b7633602239f4d5219afa
Jieyu Lu <jerrylujieyu@gmail.com>
2017-01-21 20:31:30 -0500

    Added problem 141.

---
+++ src/LinkedListCycle.java
@@ -0,0 +1,32 @@
+/**
+ * <h1>141. Linked List Cycle</h1>
+ * Given a linked list, determine if it has a cycle in it.
+ * <p>
+ * Follow up: Can you solve it without using extra space?
+ *
+ * @author Jerry
+ * @since 1/21/17
+ */
+public class LinkedListCycle {
+    public boolean hasCycle(ListNode head) {
+        ListNode walker = head, runner = head;
+        while (runner != null && runner.next != null) {
+            walker = walker.next;
+            runner = runner.next.next;
+            if (walker == runner) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
+}
