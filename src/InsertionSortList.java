fed0a65f0d62c63b1542dd48fc7bc003f1a9f6ad
Jieyu Lu <jerrylujieyu@gmail.com>
2017-01-22 16:21:26 -0500

    Added problem 147.

---
+++ src/InsertionSortList.java
@@ -0,0 +1,33 @@
+/**
+ * <h1>147. Insertion Sort List</h1>
+ * Sort a linked list using insertion sort.
+ *
+ * @author Jerry
+ * @since 1/22/17
+ */
+public class InsertionSortList {
+    public ListNode insertionSortList(ListNode head) {
+        ListNode dummy = new ListNode(0);
+        while (head != null) {
+            ListNode curr = head;
+            head = head.next;
+
+            ListNode prev = dummy;
+            while (prev.next != null && curr.val > prev.next.val) {
+                prev = prev.next;
+            }
+            curr.next = prev.next;
+            prev.next = curr;
+        }
+        return dummy.next;
+    }
+
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+}
