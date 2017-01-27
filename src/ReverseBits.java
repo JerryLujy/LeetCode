c4eeec305dbffff93d4b13a7b73f398287c2feec
Jieyu Lu <jerrylujieyu@gmail.com>
2017-01-26 20:59:28 -0500

    Added problem 190.

---
+++ src/ReverseBits.java
@@ -0,0 +1,30 @@
+/**
+ * <h1>190. Reverse Bits</h1>
+ * Reverse bits of a given 32 bits unsigned integer.
+ * <p>
+ * For example, given input 43261596 (represented in binary as 00000010100101000001111010011100),
+ * return 964176192 (represented in binary as 00111001011110000010100101000000).
+ *
+ * @author Jerry
+ * @since 1/26/17
+ */
+public class ReverseBits {
+    public int reverseBits(int n) {
+        int ans = 0;
+        for (int i = 0; i < 32; i++) {
+            ans <<= 1;
+            ans += (n & 1);
+            n >>>= 1;
+        }
+        return ans;
+    }
+
+    public int reverseBitsSmart(int n) {
+        n = (n >>> 16) | (n << 16);
+        n = ((n & 0xff00ff00) >>> 8) | ((n & 0x00ff00ff) << 8);
+        n = ((n & 0xf0f0f0f0) >>> 4) | ((n & 0x0f0f0f0f) << 4);
+        n = ((n & 0xcccccccc) >>> 2) | ((n & 0x33333333) << 2);
+        n = ((n & 0xaaaaaaaa) >>> 1) | ((n & 0x55555555) << 1);
+        return n;
+    }
+}
