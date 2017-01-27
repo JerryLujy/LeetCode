879ed0aa9ffd60eac505ebd40361c15ff9565f4f
Jieyu Lu <jerrylujieyu@gmail.com>
2017-01-26 21:10:14 -0500

    Added problem 191.

---
+++ src/NumberOf1Bits.java
@@ -0,0 +1,19 @@
+/**
+ * <h1>191. Number of 1 Bits</h1>
+ * Write a function that takes an unsigned integer and returns the number of ’1' bits it has (also known as the Hamming weight).
+ * <p>
+ * For example, the 32-bit integer ’11' has binary representation 00000000000000000000000000001011, so the function should return 3.
+ *
+ * @author Jerry
+ * @since 1/26/17
+ */
+public class NumberOf1Bits {
+    public int hammingWeight(int n) {
+        int ans = 0;
+        while (n != 0) {
+            n &= (n - 1);
+            ans++;
+        }
+        return ans;
+    }
+}
