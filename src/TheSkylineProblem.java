2b228ed75c642b894917a43492223804803be840
Jieyu Lu <jerrylujieyu@gmail.com>
2017-01-31 00:22:58 -0500

    Added problem 218.

---
+++ src/TheSkylineProblem.java
@@ -0,0 +1,62 @@
+import java.util.*;
+
+/**
+ * <h1>218. The Skyline Problem</h1>
+ * A city's skyline is the outer contour of the silhouette formed by all the buildings in that city when viewed from a distance.
+ * Now suppose you are given the locations and height of all the buildings,
+ * write a program to output the skyline formed by these buildings collectively.
+ * <p>
+ * The geometric information of each building is represented by a triplet of integers [Li, Ri, Hi],
+ * where Li and Ri are the x coordinates of the left and right edge of the ith building, respectively, and Hi is its height.
+ * It is guaranteed that 0 <= Li, Ri <= INT_MAX, 0 < Hi <= INT_MAX, and Ri - Li > 0.
+ * You may assume all buildings are perfect rectangles grounded on an absolutely flat surface at height 0.
+ * <p>
+ * For instance, the dimensions of all buildings are recorded as: [[2 9 10],[3 7 15],[5 12 12],[15 20 10],[19 24 8]].
+ * The output is a list of "key points" in the format of [[x1,y1],[x2, y2],[x3, y3],...] that uniquely defines a skyline.
+ * A key point is the left endpoint of a horizontal line segment. Note that the last key point, where the rightmost building ends,
+ * is merely used to mark the termination of the skyline, and always has zero height.
+ * Also, the ground in between any two adjacent buildings should be considered part of the skyline contour.
+ * For instance, the skyline above should be represented as:[[2 10],[3 15],[7 12],[12 0],[15 10],[20 8],[24, 0]].
+ * <p>
+ * Notes:
+ * <p>
+ * The number of buildings in any input list is guaranteed to be in the range [0, 10000].
+ * The input list is already sorted in ascending order by the left x position Li.
+ * The output list must be sorted by the x position.
+ * There must be no consecutive horizontal lines of equal height in the output skyline.
+ * For instance, [...[2 3],[4 5],[7 5],[11 5],[12 7]...] is not acceptable;
+ * the three lines of height 5 should be merged into one in the final output as such: [...[2 3],[4 5],[12 7],...]
+ *
+ * @author Jerry
+ * @since 1/30/17
+ */
+public class TheSkylineProblem {
+    public List<int[]> getSkyline(int[][] buildings) {
+        List<int[]> points = new ArrayList<>();
+        for (int[] building : buildings) {
+            points.add(new int[]{building[0], building[2]});
+            points.add(new int[]{building[1], -building[2]});
+        }
+        Collections.sort(points, (int[] p1, int[] p2) -> {
+            int posCompare = Integer.compare(p1[0], p2[0]);
+            return posCompare == 0 ? -Integer.compare(p1[1], p2[1]) : posCompare;// Rising edge comes before falling edge
+        });
+
+        List<int[]> ans = new ArrayList<>();
+        Queue<Integer> heights = new PriorityQueue<>(Collections.reverseOrder());// Keeps the maximum height
+        heights.offer(0);// Base of the skyline is at height = 0
+        int prev = 0;
+        for (int[] point : points) {
+            if (point[1] > 0) {
+                heights.offer(point[1]);// Rising edge
+            } else {
+                heights.remove(-point[1]);// Falling edge
+            }
+            if (heights.peek() != prev) {
+                ans.add(new int[]{point[0], heights.peek()});
+                prev = heights.peek();
+            }
+        }
+        return ans;
+    }
+}
