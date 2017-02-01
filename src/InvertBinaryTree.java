2a0031c7fb374d79603a78975ed3058d07a2b80f
Jieyu Lu <jerrylujieyu@gmail.com>
2017-02-01 01:40:05 -0500

    Added problem 226.

---
+++ src/InvertBinaryTree.java
@@ -0,0 +1,43 @@
+/**
+ * <h1>226. Invert Binary Tree</h1>
+ * Invert a binary tree.
+ * <pre>
+ *      4
+ *    /   \
+ *   2     7
+ *  / \   / \
+ * 1   3 6   9
+ * </pre>
+ * to
+ * <pre>
+ *      4
+ *    /   \
+ *   7     2
+ *  / \   / \
+ * 9   6 3   1
+ * </pre>
+ *
+ * @author Jerry
+ * @since 1/31/17
+ */
+public class InvertBinaryTree {
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+        TreeNode left = root.left;
+        root.left = invertTree(root.right);
+        root.right = invertTree(left);
+        return root;
+    }
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+}
