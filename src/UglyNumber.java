431e187b1a44de5a6712bbf859eef408ba352362
Jieyu Lu <jerrylujieyu@gmail.com>
2017-02-05 13:54:32 -0500

    Added problem 263.

---
+++ src/UglyNumber.java
@@ -0,0 +1,27 @@
+/**
+ * <h1>263. Ugly Number</h1>
+ * Write a program to check whether a given number is an ugly number.
+ * <p>
+ * Ugly numbers are positive numbers whose prime factors only include 2, 3, 5. For example, 6, 8 are ugly while 14 is not ugly since it includes another prime factor 7.
+ * Note that 1 is typically treated as an ugly number.
+ *
+ * @author Jerry
+ * @since 2/5/17
+ */
+public class UglyNumber {
+    public boolean isUgly(int num) {
+        if (num == 0) {
+            return false;
+        }
+        while (num % 2 == 0) {
+            num /= 2;
+        }
+        while (num % 3 == 0) {
+            num /= 3;
+        }
+        while (num % 5 == 0) {
+            num /= 5;
+        }
+        return num == 1;
+    }
+}
