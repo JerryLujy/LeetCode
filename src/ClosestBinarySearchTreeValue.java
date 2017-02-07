246c19b1ce9307d0bdbf18d530d8113a838ea0df
Jieyu Lu <jerrylujieyu@gmail.com>
2017-02-06 22:27:46 -0500

    Added problem 270.

---
+++ src/ClosestBinarySearchTreeValue.java
@@ -0,0 +1,31 @@
+/**
+ * <h1>270. Closest Binary Search Tree Value</h1>
+ * Given a non-empty binary search tree and a target value, find the value in the BST that is closest to the target.
+ * <p>
+ * Note:
+ * Given target value is a floating point.
+ * You are guaranteed to have only one unique value in the BST that is closest to the target.
+ *
+ * @author Jerry
+ * @since 2/6/17
+ */
+public class ClosestBinarySearchTreeValue {
+    public int closestValue(TreeNode root, double target) {
+        TreeNode next = root.val > target ? root.left : root.right;
+        if (next == null) {
+            return root.val;
+        }
+        int closest = closestValue(next, target);
+        return Math.abs(root.val - target) > Math.abs(closest - target) ? closest : root.val;
+    }
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+}
