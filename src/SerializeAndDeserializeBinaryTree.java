cd09d9c19c4168e6cc4ae24dbe35248e235ac21f
Jieyu Lu <jerrylujieyu@gmail.com>
2017-02-12 15:33:41 -0500

    Added problem 297.

---
+++ src/SerializeAndDeserializeBinaryTree.java
@@ -0,0 +1,73 @@
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Queue;
+
+/**
+ * <h1>297. Serialize and Deserialize Binary Tree</h1>
+ * Serialization is the process of converting a data structure or object into a sequence of bits so that it can be stored in a file or memory buffer,
+ * or transmitted across a network connection link to be reconstructed later in the same or another computer environment.
+ * <p>
+ * Design an algorithm to serialize and deserialize a binary tree. There is no restriction on how your serialization/deserialization algorithm should work.
+ * You just need to ensure that a binary tree can be serialized to a string and this string can be deserialized to the original tree structure.
+ * <p>
+ * For example, you may serialize the following tree
+ * <pre>
+ *   1
+ *  / \
+ * 2   3
+ *    / \
+ *   4   5
+ * </pre>
+ * as "[1,2,3,null,null,4,5]", just the same as how LeetCode OJ serializes a binary tree.
+ * You do not necessarily need to follow this format, so please be creative and come up with different approaches yourself.
+ * <p>
+ * Note: Do not use class member/global/static variables to store states. Your serialize and deserialize algorithms should be stateless.
+ *
+ * @author Jerry
+ * @since 2/12/17
+ */
+public class SerializeAndDeserializeBinaryTree {
+    // Encodes a tree to a single string.
+    public String serialize(TreeNode root) {
+        StringBuilder sb = new StringBuilder();
+        serialize(root, sb);
+        return sb.toString();
+    }
+
+    private void serialize(TreeNode root, StringBuilder sb) {
+        if (root == null) {
+            sb.append("X").append(",");
+            return;
+        }
+        sb.append(root.val).append(",");
+        serialize(root.left, sb);
+        serialize(root.right, sb);
+    }
+
+    // Decodes your encoded data to tree.
+    public TreeNode deserialize(String data) {
+        Queue<String> tokens = new ArrayDeque<>(Arrays.asList(data.split(",")));
+        return deserialize(tokens);
+    }
+
+    private TreeNode deserialize(Queue<String> tokens) {
+        String curr = tokens.poll();
+        if (curr.equals("X")) {
+            return null;
+        }
+        TreeNode newNode = new TreeNode(Integer.parseInt(curr));
+        newNode.left = deserialize(tokens);
+        newNode.right = deserialize(tokens);
+        return newNode;
+    }
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+}
