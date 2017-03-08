b4e30dc004d439854964db0945653825eaa77939
Jieyu Lu <jerrylujieyu@gmail.com>
2017-03-08 13:46:34 -0500

    Updated problem 94.

--- src/BinaryTreeInorderTraversal.java
+++ src/BinaryTreeInorderTraversal.java
@@ -1,3 +1,5 @@
+import support.TreeNode;
+
 import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.Deque;
@@ -8,11 +10,13 @@ import java.util.List;
  * Given a binary tree, return the inorder traversal of its nodes' values.
  * <p>
  * For example: Given binary tree [1,null,2,3],
+ * <pre>
  * 1
- * \
- * 2
- * /
+ *  \
+ *   2
+ *  /
  * 3
+ * </pre>
  * return [1,3,2].
  *
  * @author Jerry
@@ -24,23 +28,37 @@ public class BinaryTreeInorderTraversal {
         Deque<TreeNode> stack = new ArrayDeque<>();
         while (root != null || !stack.isEmpty()) {
             while (root != null) {
-                stack.offerLast(root);
+                stack.push(root);
                 root = root.left;
             }
-            root = stack.pollLast();
+            root = stack.pop();
             ans.add(root.val);
             root = root.right;
         }
         return ans;
     }
 
-    public class TreeNode {
-        int val;
-        TreeNode left;
-        TreeNode right;
-
-        TreeNode(int x) {
-            val = x;
+    public List<Integer> inorderTraversalMorris(TreeNode root) {
+        List<Integer> ans = new ArrayList<>();
+        while (root != null) {
+            if (root.left == null) {
+                ans.add(root.val);
+                root = root.right;
+            } else {
+                TreeNode pred = root.left;
+                while (pred.right != null && pred.right != root) {
+                    pred = pred.right;
+                }
+                if (pred.right == null) {
+                    pred.right = root;
+                    root = root.left;
+                } else {
+                    ans.add(root.val);
+                    pred.right = null;
+                    root = root.right;
+                }
+            }
         }
+        return ans;
     }
 }
