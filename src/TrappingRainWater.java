ca8d638ccceed9b5f4f99582869ec8ae7198c8be
Jieyu Lu <jerrylujieyu@gmail.com>
2017-03-15 15:08:37 -0400

    Updated problem 42.

--- src/TrappingRainWater.java
+++ src/TrappingRainWater.java
@@ -1,3 +1,6 @@
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 /**
  * <h1>42. Trapping Rain Water</h1>
  * Given n non-negative integers representing an elevation map where the width of each bar is 1, compute how much water it is able to trap after raining.
@@ -29,5 +32,24 @@ public class TrappingRainWater {
             }
         }
         return amount;
+    }
+
+    public int trap2(int[] height) {
+        if (height == null || height.length < 3) {
+            return 0;
+        }
+        Deque<Integer> stack = new ArrayDeque<>();
+        int ans = 0;
+        for (int i = 0; i < height.length; i++) {
+            while (!stack.isEmpty() && height[stack.peek()] <= height[i]) {
+                int prevHeight = height[stack.pop()];
+                if (!stack.isEmpty()) {
+                    int waterLevel = Math.min(height[stack.peek()], height[i]);
+                    ans += (waterLevel - prevHeight) * (i - stack.peek() - 1); // height * width
+                }
+            }
+            stack.push(i);
+        }
+        return ans;
     }
 }
