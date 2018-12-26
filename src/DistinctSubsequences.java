495ef25c53604efc3833f2e40cfcecb059efc7c3
Jieyu Lu <jerrylujieyu@gmail.com>
2018-12-26 00:58:01 -0800

    Updated problem 115.

--- src/DistinctSubsequences.java
+++ src/DistinctSubsequences.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 /**
  * <h1>115. Distinct Subsequences</h1>
  * Given a string S and a string T, count the number of distinct subsequences of T in S.
@@ -16,21 +18,16 @@ public class DistinctSubsequences {
         if (s.length() == 0 || t.length() == 0 || s.length() < t.length()) {
             return 0;
         }
-        int[][] dp = new int[t.length()][s.length()];
-        dp[0][0] = s.charAt(0) == t.charAt(0) ? 1 : 0;
-        for (int i = 1; i < s.length(); i++) {
-            dp[0][i] = dp[0][i - 1] + (s.charAt(i) == t.charAt(0) ? 1 : 0);
-        }
-        for (int i = 1; i < t.length(); i++) {
-            dp[i][i] = (dp[i - 1][i - 1] == 1 && s.charAt(i) == t.charAt(i)) ? 1 : 0;
-            for (int j = i; j < s.length(); j++) {
-                if (t.charAt(i) == s.charAt(j)) {
-                    dp[i][j] = dp[i][j - 1] + dp[i - 1][j - 1];
-                } else {
-                    dp[i][j] = dp[i][j - 1];
+        int[][] mem = new int[t.length() + 1][s.length() + 1];
+        Arrays.fill(mem[0], 1);
+        for (int i = 1; i <= t.length(); i++) {
+            for (int j = i; j <= s.length(); j++) {
+                mem[i][j] = mem[i][j - 1];
+                if (t.charAt(i - 1) == s.charAt(j - 1)) {
+                    mem[i][j] += mem[i - 1][j - 1];
                 }
             }
         }
-        return dp[t.length() - 1][s.length() - 1];
+        return mem[t.length()][s.length()];
     }
 }
