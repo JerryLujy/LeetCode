b823f54f5015d9fd6448e852534ba7926ddfd3bd
Jieyu Lu <jerrylujieyu@gmail.com>
2019-01-04 15:04:40 +0800

    Updated problem 239.

--- src/SlidingWindowMaximum.java
+++ src/SlidingWindowMaximum.java
@@ -32,12 +32,11 @@ public class SlidingWindowMaximum {
         int[] ans = new int[nums.length - k + 1];
         Deque<Integer> deque = new ArrayDeque<>();
         for (int i = 0; i < nums.length; i++) {
-            while (!deque.isEmpty() && deque.peekFirst() < i - k + 1) {
-                // Remove out of range indices
+            if (!deque.isEmpty() && deque.peekFirst() == i - k) {
                 deque.pollFirst();
             }
             while (!deque.isEmpty() && nums[deque.peekLast()] < nums[i]) {
-                // Remove indices that is impossible to becoming maximum
+                // Remove indices that is impossible to become maximum
                 deque.pollLast();
             }
             deque.offerLast(i);
