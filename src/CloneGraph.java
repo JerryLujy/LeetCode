ad1541e26a931b938d21aee24cd95aa0ddba7bae
Jieyu Lu <jerrylujieyu@gmail.com>
2019-01-11 22:08:27 +0800

    Updated problem 133.

--- src/CloneGraph.java
+++ src/CloneGraph.java
@@ -8,33 +8,45 @@ import java.util.*;
  * @since 1/20/17
  */
 public class CloneGraph {
-    public UndirectedGraphNode cloneGraph(UndirectedGraphNode node) {
+    public UndirectedGraphNode cloneGraphBFS(UndirectedGraphNode node) {
         if (node == null) {
             return null;
         }
         Map<UndirectedGraphNode, UndirectedGraphNode> oldToNew = new HashMap<>();
-        Set<UndirectedGraphNode> visited = new HashSet<>();
+        oldToNew.put(node, new UndirectedGraphNode(node.label));
         Queue<UndirectedGraphNode> queue = new ArrayDeque<>();
         queue.offer(node);
         while (!queue.isEmpty()) {
-            UndirectedGraphNode curr = queue.poll();
-            if (visited.contains(curr)) {
-                continue;
-            }
-            visited.add(curr);
-            if (!oldToNew.containsKey(curr)) {
-                oldToNew.put(curr, new UndirectedGraphNode(curr.label));
-            }
-            UndirectedGraphNode currDup = oldToNew.get(curr);
-            for (UndirectedGraphNode neighbor : curr.neighbors) {
-                if (!oldToNew.containsKey(neighbor)) {
-                    oldToNew.put(neighbor, new UndirectedGraphNode(neighbor.label));
+            UndirectedGraphNode cur = queue.poll();
+            for (UndirectedGraphNode next : cur.neighbors) {
+                if (!oldToNew.containsKey(next)) {
+                    oldToNew.put(next, new UndirectedGraphNode(next.label));
+                    queue.offer(next);
                 }
-                currDup.neighbors.add(oldToNew.get(neighbor));
-                queue.offer(neighbor);
+                oldToNew.get(cur).neighbors.add(oldToNew.get(next));
             }
         }
         return oldToNew.get(node);
+    }
+
+    public UndirectedGraphNode cloneGraphDFS(UndirectedGraphNode node) {
+        if (node == null) {
+            return null;
+        }
+        Map<UndirectedGraphNode, UndirectedGraphNode> oldToNew = new HashMap<>();
+        return dfs(node, oldToNew);
+    }
+
+    private UndirectedGraphNode dfs(UndirectedGraphNode node, Map<UndirectedGraphNode, UndirectedGraphNode> oldToNew) {
+        if (oldToNew.containsKey(node)) {
+            return oldToNew.get(node);
+        }
+        UndirectedGraphNode newNode = new UndirectedGraphNode(node.label);
+        oldToNew.put(node, newNode);
+        for (UndirectedGraphNode next : node.neighbors) {
+            newNode.neighbors.add(dfs(next, oldToNew));
+        }
+        return newNode;
     }
 
     class UndirectedGraphNode {
