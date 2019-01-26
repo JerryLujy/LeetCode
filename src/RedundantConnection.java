cc6f9282f48242d0c387b201724ef25bd6e1955c
Jieyu Lu <jerrylujieyu@gmail.com>
2019-01-25 20:25:25 -0800

    Added problem 684.

---
+++ src/RedundantConnection.java
@@ -0,0 +1,78 @@
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * <h1>684. Redundant Connection</h1>
+ * In this problem, a tree is an undirected graph that is connected and has no cycles.
+ * The given input is a graph that started as a tree with N nodes (with distinct values 1, 2, ..., N),
+ * with one additional edge added. The added edge has two different vertices chosen from 1 to N, and was not an edge that already existed.
+ * The resulting graph is given as a 2D-array of edges. Each element of edges is a pair [u, v] with u < v, that represents an undirected edge connecting nodes u and v.
+ * Return an edge that can be removed so that the resulting graph is a tree of N nodes.
+ * If there are multiple answers, return the answer that occurs last in the given 2D-array. The answer edge [u, v] should be in the same format, with u < v.
+ * <p>
+ * Example 1:
+ * Input: [[1,2], [1,3], [2,3]]
+ * Output: [2,3]
+ * Explanation: The given undirected graph will be like this:
+ *   1
+ *  / \
+ * 2 - 3
+ * <p>
+ * Example 2:
+ * Input: [[1,2], [2,3], [3,4], [1,4], [1,5]]
+ * Output: [1,4]
+ * Explanation: The given undirected graph will be like this:
+ * 5 - 1 - 2
+ *     |   |
+ *     4 - 3
+ * <p>
+ * Note:
+ * The size of the input 2D-array will be between 3 and 1000.
+ * Every integer represented in the 2D-array will be between 1 and N, where N is the size of the input array.
+ *
+ * @author Jerry
+ * @since 1/25/19
+ */
+public class RedundantConnection {
+    public int[] findRedundantConnection(int[][] edges) {
+        int n = 0;
+        for (int[] edge : edges) {
+            n = Math.max(n, Math.max(edge[0], edge[1]));
+        }
+        UnionFind uf = new UnionFind(n);
+        for (int[] edge : edges) {
+            if (uf.find(edge[0]) == uf.find(edge[1])) {
+                return edge;
+            }
+            uf.union(edge[0], edge[1]);
+        }
+        return new int[0];
+    }
+
+    private static class UnionFind {
+        Map<Integer, Integer> fathers;
+        UnionFind(int n) {
+            fathers = new HashMap<>();
+            for (int i = 1; i <= n; i++) {
+                fathers.put(i, i);
+            }
+        }
+
+        int find(int i) {
+            int ans = i;
+            while (fathers.get(ans) != ans) {
+                ans = fathers.get(ans);
+            }
+            while (i != ans) {
+                int father = fathers.get(i);
+                fathers.put(i, ans);
+                i = father;
+            }
+            return ans;
+        }
+
+        void union(int i, int j) {
+            fathers.put(find(i), find(j));
+        }
+    }
+}
