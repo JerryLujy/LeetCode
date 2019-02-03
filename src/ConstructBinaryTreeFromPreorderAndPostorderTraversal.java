f7fee90b794597072c5be93306055421220bb4d9
Jieyu Lu <jerrylujieyu@gmail.com>
2019-02-03 00:25:39 -0800

    Updated problem 889.

--- src/ConstructBinaryTreeFromPreorderAndPostorderTraversal.java
+++ src/ConstructBinaryTreeFromPreorderAndPostorderTraversal.java
@@ -4,20 +4,16 @@ import support.TreeNode;
  * <h1>889. Construct Binary Tree from Preorder and Postorder Traversal</h1>
  * Return any binary tree that matches the given preorder and postorder traversals.
  * Values in the traversals pre and post are distinct positive integers.
- *
- * Example 1:
  * <p>
- * Input: pre = [1,2,4,5,3,6,7], post = [4,5,2,6,7,3,1]
- * Output: [1,2,3,4,5,6,7]
+ * Example 1: Input: pre = [1,2,4,5,3,6,7], post = [4,5,2,6,7,3,1]. Output: [1,2,3,4,5,6,7]
  * <p>
  * Note:
- * <p>
  * 1 <= pre.length == post.length <= 30
- * <p>
  * pre[] and post[] are both permutations of 1, 2, ..., pre.length.
- * <p>
  * It is guaranteed an answer exists. If there exists multiple answers, you can return any of them.
  *
+ * @author Jerry
+ * @since 12/28/18
  */
 public class ConstructBinaryTreeFromPreorderAndPostorderTraversal {
     public TreeNode constructFromPrePost(int[] pre, int[] post) {
