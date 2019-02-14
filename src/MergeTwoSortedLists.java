500aa39d1dfe3a8027a9995bde01a8260e04bcc8
Jieyu Lu <jerrylujieyu@gmail.com>
2019-02-13 23:29:02 -0800

    Updated problem 21.

--- src/MergeTwoSortedLists.java
+++ src/MergeTwoSortedLists.java
@@ -10,21 +10,7 @@ import support.ListNode;
  */
 public class MergeTwoSortedLists {
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-        if (l1 == null) {
-            return l2;
-        }
-        if (l2 == null) {
-            return l1;
-        }
-        ListNode rst;
-        if (l1.val < l2.val) {
-            rst = l1;
-            l1 = l1.next;
-        } else {
-            rst = l2;
-            l2 = l2.next;
-        }
-        ListNode cur = rst;
+        ListNode dummy = new ListNode(0), cur = dummy;
         while (l1 != null && l2 != null) {
             if (l1.val < l2.val) {
                 cur.next = l1;
@@ -40,6 +26,6 @@ public class MergeTwoSortedLists {
         } else {
             cur.next = l2;
         }
-        return rst;
+        return dummy.next;
     }
 }
