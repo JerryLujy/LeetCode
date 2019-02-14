9239cdbf16361eb625cb831ea47d278cda9f6408
Jieyu Lu <jerrylujieyu@gmail.com>
2019-02-13 23:09:42 -0800

    Updated problem 35.

--- src/SearchInsertPosition.java
+++ src/SearchInsertPosition.java
@@ -19,18 +19,11 @@
  */
 public class SearchInsertPosition {
     public int searchInsert(int[] nums, int target) {
-        if (nums == null || nums.length == 0) {
-            return 0;
-        }
-        int l = 0;
-        int r = nums.length - 1;
-        int m;
-        while (l <= r) {
-            m = l + (r - l) / 2;
-            if (nums[m] == target) {
-                return m;
-            } else if (nums[m] > target) {
-                r = m - 1;
+        int l = 0, r = nums.length;
+        while (l < r) {
+            int m = l + (r - l) / 2;
+            if (nums[m] >= target) {
+                r = m;
             } else {
                 l = m + 1;
             }
